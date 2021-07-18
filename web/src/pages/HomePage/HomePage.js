fe11fdb2a66a12ceccf233654ac506fdcfa3b725
moorels <sales@micropls.com>
2021-07-18 12:36:57 +1000

    sdfs

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,12 +1,19 @@
 import { Link, routes } from '@redwoodjs/router'
+import { useState, useEffect } from 'react'
 
 const HomePage = () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount(count + 1)
+    }, 1000)
+  }, [count])
+
   return (
     <>
       <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
+      <p>{count}</p>
       <p>
         Tester .<Link to={routes.home()}>Home</Link>`
       </p>
