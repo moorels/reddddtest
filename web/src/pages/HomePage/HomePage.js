fdcd500c89882df8b50f76630f570ee9b9c4fd81
moorels <sales@micropls.com>
2021-07-18 12:25:09 +1000

    ghj

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>HomePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
+      </p>
+      <p>
+        Tester .<Link to={routes.home()}>Home</Link>`
+      </p>
+    </>
+  )
+}
+
+export default HomePage
